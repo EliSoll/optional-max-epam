839f1bac68df628c500934fff31cd80790ca0d29
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-08-26 17:13:49 +0300

    t

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,9 +1,39 @@
 package com.epam.rd.autotasks.max;
+import java.util.Optional;
 
 import java.util.OptionalInt;
 
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
+
+        int max = 0;
+        if(values != null) {
+            for (int num : values) {
+                if (num > max) {
+                    max = num;
+                }
+            }
+        }
+return maxOpt(values);
+    }
+    public static OptionalInt maxOpt(int[] values){
+        OptionalInt result;
+        if(values == null) {
+            return OptionalInt.empty();
+        }
+        if(values != null && values.length > 0) {
+            int max = values[0];
+            for (int num : values) {
+                if(num > max) {
+                    max = num;
+                }
+            }
+         result = OptionalInt.of(max);
+        }
+        else {
+            result = OptionalInt.empty();
+        }
+return result;
     }
 }
+
